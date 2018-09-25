a08de7f8f79eea09d5ee087253faf67fd2dd6775
Manuel Spigolon <behemoth89@gmail.com>
2018-09-25 21:54:17 +0200

    First try of claudiajs

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ fastify.register(require('./api/ping'));
 fastify.register(require('./api/vision'));
 fastify.register(require('./api/profile-picture'));
 
+if (require.main === module) {
+  fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`)
+  });
+} else {
+  module.exports = fastify;
+}
 
-fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`server listening on ${address}`)
-});
